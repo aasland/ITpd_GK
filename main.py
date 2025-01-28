26ae1d51c2ff72caaa5fd85fcc6076d4e0a8b051
aArntzen <158445429+aArntzen@users.noreply.github.com>
2025-01-28 16:14:31 +0100

    Update main.py

---
+++ main.py
@@ -0,0 +1,39 @@
+import pygame
+from karakterer import *
+from settings import *
+import sys
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+        self.clock = pygame.time.Clock()
+        self.font = pygame.font.Font("Arial", 20)
+        self.running = True
+
+    def new(self):
+        self.playing = True
+        self.all_sprites = pygame.sprite.LayeredUpdates()
+        self.blocks = pygame.sprite.LayeredUpdates()
+        self.enemies = pygame.sprite.LayeredUpdates()
+        self.attacks = pygame.sprite.LayeredUpdates()
+
+        self.player = Player(self, 1, 2)
+
+    def events(self):
+    
+    def update(self):
+
+    def draw(self):
+
+    def main(self):
+
+        while self.playing:
+            self.events()
+            self.update()
+            self.draw()
+        self.running = False
+    
+    def game_over(self):
+    
+    def intro_screen(self):
