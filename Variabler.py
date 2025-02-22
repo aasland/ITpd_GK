5b74b1824b3379fcc48e54ee5dcbe55a4c2ec913
aArntzen <158445429+aArntzen@users.noreply.github.com>
2025-02-22 18:58:08 +0100

    Update Variabler.py

--- Variabler.py
+++ Variabler.py
@@ -2,7 +2,8 @@ WIN_WIDTH = 640
 WIN_HEIGHT = 480
 TILESIZE = 32
 
-PLAYER_LAYER = 4
+SHEEP_LAYER = 4
+PLAYER_LAYER = 5
 ENEMY_LAYER = 3
 GROUND_LAYER = 1
 BLOCK_LAYER = 2
@@ -10,6 +11,7 @@ FPS = 60
 
 PLAYER_SPEED = 2
 ENEMY_SPEED = 1
+SHEEP_SPEED = 1
 
 BLUE = (0,0,255)
 RED = (255, 0, 0)
@@ -19,18 +21,18 @@ GRAY = (200, 200, 200)
 
 tilemap = [
     "BBBBBBBBBBBBBBBBBBBB",
-    "B..E...............B",
-    "B.............BB...B",
+    "B..E......S......GGB",
+    "B.............BB.GGB",
     "B....BBBBB.........B",
-    "B..................B",
+    "B............E.....B",
     "B........P.........B",
-    "B..................B",
+    "B..S...............B",
     "B.............B....B",
     "B.....BBB..........B",
     "B.......B..........B",
-    "B.......B.....E....B",
+    "B..S....B.....E....B",
     "B..................B",
-    "B..................B",
-    "B..................B",
+    "B..........S.......B",
+    "B...S..............B",
     "BBBBBBBBBBBBBBBBBBBB",
 ]
